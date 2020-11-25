28123050738ec6c4b73d5cf67ea869feddc9e802
marc lopes <marc_lopes2004@yahoo.fr>
2020-11-25 16:13:08 +0900

    set controller action route and view to display list of books

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Book;
 
 class BookController extends Controller
 {
-    //
+    // show list of books ordered by creation date or a searched list
+    public function index(Request $request)
+    {
+      if ($request->has('search'))
+      {
+        $request->flashOnly('search');
+        $search = $request->input('search');
+        $books = $this->search_books($search);
+      }
+      else
+      {
+        $books = Book::all()->sortDesc();
+      }
+      return view('books.index', ['books' => $books]);
+    }
 }
