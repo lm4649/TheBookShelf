c852855eba0d802ce0b444f16ec0d0ba6810b77c
marc lopes <marc_lopes2004@yahoo.fr>
2020-11-25 21:33:37 +0900

    adjusted temporarily asset loading to work in dev env

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>The Bookshelf</title>
     <!-- Scripts -->
-    <script src="{{ secure_asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
     <!-- Styles -->
-    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   </head>
   <body>
     {{-- add-form modal shall be placed on top the body --}}
