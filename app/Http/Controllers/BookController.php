2b3743c032668cbd2b349c454cd369247a0d8afa
marc lopes <marc_lopes2004@yahoo.fr>
2020-11-25 16:23:50 +0900

    in BookController implemented the search function for the index action

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Book;
+use Illuminate\Support\Facades\DB;
 
 class BookController extends Controller
 {
@@ -21,5 +22,27 @@ class BookController extends Controller
         $books = Book::all()->sortDesc();
       }
       return view('books.index', ['books' => $books]);
+    }
+
+    //-----------------------PRIVATE METHODS----------------------------------//
+
+    protected function search_books($term, $order = 'created_at')
+    {
+      $search = '%' . trim($term) . '%';
+      $reversed_search = '%' . trim($this->reverse_words($term)) . '%';
+      $books = DB::table('books')->distinct()
+                                  ->where('title','like', $search)
+                                  ->orWhere('title', 'like', $reversed_search)
+                                  ->orWhere('author', 'like', $search)
+                                  ->orWhere('author', 'like', $reversed_search)
+                                  ->orderBy($order)
+                                  ->get();
+      return $books;
+    }
+
+    protected function reverse_words(string $term)
+    {
+      $search_terms = explode( ' ', $term);
+      return join(" ", array_reverse($search_terms));
     }
 }
